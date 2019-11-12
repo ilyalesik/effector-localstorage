4ed71989d2cbb9e0f26d8bb85c5a6cc124d54f8b
yumauri <yumaa.verdin@gmail.com>
2019-11-12 21:04:33 +0300

    Reduce size / Add support of sessionStorage

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module "effector-localstorage" {
   interface Holder {
-    init: (def?: any) => any
+    init: (value?: any) => any
     onError: (handler: (err: any) => void) => Holder
     (storeValue: any): Holder
   }
 
-  export default function connectLocalStorage(key: string): Holder
+  export default function connectLocalStorage(key: string, storage?: Storage): Holder
 }
