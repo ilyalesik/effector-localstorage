436046622a718ebbd549446627e3d4fe67e3b9f3
yumauri <yumaa.verdin@gmail.com>
2019-11-13 17:16:24 +0300

    Add 'storage' event support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function connectLocalStorage (key, storage) {
+function connectStorage (key, storage) {
   storage = storage || window.localStorage
   var errorHandler = function () {} // eslint-disable-line func-style
 
@@ -15,6 +15,13 @@ function connectLocalStorage (key, storage) {
     return holder
   }
 
+  holder.onChange = function (event) {
+    window.addEventListener('storage', function (e) {
+      e.key === key && e.storageArea === storage && event(holder.init())
+    })
+    return holder
+  }
+
   holder.init = function (value) {
     try {
       value = JSON.parse(storage.getItem(key))
@@ -27,4 +34,4 @@ function connectLocalStorage (key, storage) {
   return holder
 }
 
-module.exports = connectLocalStorage
+module.exports = connectStorage
