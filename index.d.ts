bb812812c8cd0910e16dbe94861bdab883887932
Victor Didenko <victord@setplex.com>
2023-07-10 20:49:14 +0300

    Remove old stuff

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "effector-localstorage" {
-  interface Holder {
-    init: (value?: any) => any
-    onError: (handler: (err: any) => void) => Holder
-    (storeValue: any): Holder
-  }
-
-  export default function connectLocalStorage(key: string): Holder
-}
